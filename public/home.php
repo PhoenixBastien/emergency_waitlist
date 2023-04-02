71d2fe6f6373779aeed4f344d063cac64bc81f0e
pbast074 <pbast074@uottawa.ca>
2023-04-02 15:46:01 -0400

    réparé href pour style.css

--- public/home.php
+++ public/home.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['loggedin'])) {
 	<head>
 		<meta charset="utf-8">
 		<title>Home Page</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
+		<link href="assets/style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 	</head>
 	<body class="loggedin">
