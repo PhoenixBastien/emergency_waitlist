8ef11de25cbcafb3b74a496764be813d221b3f2c
pbast074 <pbast074@uottawa.ca>
2023-04-02 15:24:39 -0400

    modifié schema.sql et seed.sql

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP SCHEMA IF EXISTS phplogin;
+CREATE SCHEMA phplogin;
+
+USE phplogin;
+
+CREATE TABLE user (
+	user_id SERIAL NOT NULL,
+  	username varchar(50) NOT NULL,
+  	user_password varchar(255) NOT NULL,
+  	email varchar(100) NOT NULL,
+	user_role varchar(7) NOT NULL,
+	arrival_time TIME,
+	severity int,
+    PRIMARY KEY (user_id)
+);
