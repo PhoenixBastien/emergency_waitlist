375c8ecf72a4b2251e58ebfcb397d85060f5322c
pbast074 <pbast074@uottawa.ca>
2023-04-02 18:54:19 -0400

    modifié seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,15 +1,15 @@
 USE phplogin;
 
 INSERT INTO user (user_id, username, user_password, email, user_role) VALUES (DEFAULT, 'test', 'test', 'test@test.com', 'admin');
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'joe', 'patient', 'joe@patient.com', 'patient', TIMESTAMP'2008-11-11 18:00:00', 5);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'sam', 'patient', 'sam@patient.com', 'patient', TIMESTAMP'2008-11-11 17:00:00', 5);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'max', 'patient', 'max@patient.com', 'patient', TIMESTAMP'2008-11-11 18:00:00', 1);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'pho', 'patient', 'pho@patient.com', 'patient', TIMESTAMP'2008-11-11 19:00:00', 2);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'mel', 'patient', 'mel@patient.com', 'patient', TIMESTAMP'2008-11-11 17:00:00', 3);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'jay', 'patient', 'jay@patient.com', 'patient', TIMESTAMP'2008-11-11 16:00:00', 1);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'jon', 'patient', 'jon@patient.com', 'patient', TIMESTAMP'2008-11-11 16:00:00', 2);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'ron', 'patient', 'ron@patient.com', 'patient', TIMESTAMP'2008-11-11 16:00:00', 3);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'don', 'patient', 'don@patient.com', 'patient', TIMESTAMP'2008-11-11 16:00:00', 4);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'ray', 'patient', 'ray@patient.com', 'patient', TIMESTAMP'2008-11-11 16:00:00', 5);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'kev', 'patient', 'kev@patient.com', 'patient', TIMESTAMP'2008-11-11 21:00:00', 4);
-INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'zak', 'patient', 'zak@patient.com', 'patient', current_time(), 5);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'joe', 'joe', 'joe@patient.com', 'patient', TIMESTAMP'2023-04-02 18:00:00', 5);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'sam', 'sam', 'sam@patient.com', 'patient', TIMESTAMP'2023-04-02 17:00:00', 5);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'max', 'max', 'max@patient.com', 'patient', TIMESTAMP'2023-04-02 18:00:00', 1);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'pho', 'pho', 'pho@patient.com', 'patient', TIMESTAMP'2023-04-02 19:00:00', 2);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'mel', 'mel', 'mel@patient.com', 'patient', TIMESTAMP'2023-04-02 17:00:00', 3);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'jay', 'jay', 'jay@patient.com', 'patient', TIMESTAMP'2023-04-02 16:00:00', 1);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'jon', 'jon', 'jon@patient.com', 'patient', TIMESTAMP'2023-04-02 16:00:00', 2);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'ron', 'ron', 'ron@patient.com', 'patient', TIMESTAMP'2023-04-02 16:00:00', 3);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'don', 'don', 'don@patient.com', 'patient', TIMESTAMP'2023-04-02 16:00:00', 4);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'ray', 'ray', 'ray@patient.com', 'patient', TIMESTAMP'2023-04-02 16:00:00', 5);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'kev', 'kev', 'kev@patient.com', 'patient', TIMESTAMP'2023-04-04 21:00:00', 4);
+INSERT INTO user (user_id, username, user_password, email, user_role, arrival_time, severity) VALUES (DEFAULT, 'zak', 'zak', 'zak@patient.com', 'patient', current_time(), 5);
