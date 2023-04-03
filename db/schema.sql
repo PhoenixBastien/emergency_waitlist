3b70ae7e9eb3a71a81eea4832c9ab8e7b7dfd175
pbast074 <pbast074@uottawa.ca>
2023-04-03 09:49:42 -0400

    modifié schema.sql et seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE SCHEMA emergency_waitlist;
 USE emergency_waitlist;
 
 CREATE TABLE user (
-	user_id SERIAL NOT NULL,
-  	username varchar(50) NOT NULL,
-  	user_password varchar(255) NOT NULL,
-  	email varchar(100) NOT NULL,
-	user_role varchar(7) NOT NULL,
+	user_id SERIAL NOT NULL UNIQUE,
+  	username VARCHAR(50) NOT NULL UNIQUE,
+  	user_password VARCHAR(255) NOT NULL,
+  	email VARCHAR(100) NOT NULL UNIQUE,
+	user_role VARCHAR(7) NOT NULL,
 	arrival_time TIMESTAMP,
-	severity int,
+	severity INT,
     PRIMARY KEY (user_id)
 );
