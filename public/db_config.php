b8e6f460d1a1528c707f438299e56244fbabf248
pbast074 <pbast074@uottawa.ca>
2023-04-07 11:48:00 -0400

    changé global.php à db_config.php

---
+++ public/db_config.php
@@ -0,0 +1,8 @@
+<?php
+// Change this to your connection info
+define('HOSTNAME', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', 'password');
+define('DATABASE', 'emergency_waitlist');
+define('PORT', '3306');
+?>
--- public/global.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Change this to your connection info.
-$host = 'localhost';
-$username = 'root';
-$password = 'password';
-$dbname = 'emergency_waitlist';
-$port = '3306';
-?>
